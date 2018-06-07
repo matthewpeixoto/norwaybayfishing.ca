b994ed17f2f5b028cf2a724c40dcff40edea0b8c
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-06-07 11:34:53 -0400

    Blanket commit

--- _site/js/header.js
+++ _site/js/header.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
     $('.menu-container').slideToggle(550);
     $('.menu-item').toggleClass('js-menu-item-active');
     $('.hamburger').toggleClass('is-active');
-    $('body').toggleClass('overflow-hidden');
+    $('body, html').toggleClass('overflow-hidden');
   });
 
   $('.menu-link').click(function () {
     $('.menu-container').slideToggle(550);
     $('.menu-item').removeClass('js-menu-item-active');
     $('.hamburger').removeClass('is-active');
-    $('body').removeClass('overflow-hidden');
+    $('body, html').removeClass('overflow-hidden');
   });
 
 });
